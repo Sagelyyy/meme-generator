786cd1da46844b48fb7c6b5ecb1631117ba102b0
Chris <orj.tsuk@gmail.com>
2022-01-27 22:49:53 -0500

    Bringing in the fake json data, and making it grab a random image when the page loads.

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,14 +1,30 @@
 import React from "react";
+import memesData from "../memesData";
 
 export default function Meme(){
+
+    function getMemeImage(){
+        const memesArray = memesData.data.memes
+        const data = Math.floor(Math.random() * memesArray.length)
+        return console.log(memesArray[data].url)
+    }
+
     return(
         <div className="meme--container">
             <div className="meme--input--container">
-                <input className="meme--topText"></input>
-                <input className="meme--bottomText"></input>
+                <input 
+                className="meme--topText"
+                type="text"
+                placeholder="Top Text"
+                ></input>
+                <input 
+                className="meme--bottomText"
+                type="text"
+                placeholder="Bottom Text"
+                ></input>
             </div>
             <div className="meme--button--container">
-                <button className="meme--button">Get a new meme image  🖼 </button>
+                <button onClick={getMemeImage} className="meme--button">Get a new meme image  🖼 </button>
             </div>
         </div>
     )
