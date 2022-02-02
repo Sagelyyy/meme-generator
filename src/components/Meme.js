8b0acd342c55c0e29d18aea82e86d156852b08b3
Chris <orj.tsuk@gmail.com>
2022-02-02 08:37:20 -0500

    refactoring

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,35 +1,47 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import memesData from "../memesData";
 
-export default function Meme(){
+export default function Meme() {
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
 
-    const [memeImage, setImage] = useState('')
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
-    function getMemeImage(){
-        const memesArray = memesData.data.memes
-        const data = Math.floor(Math.random() * memesArray.length)
-        setImage(memesArray[data].url)
-        console.log(memeImage)
-    }
+  function getMemeImage() {
+    const theMemes = allMemeImages.data.memes
+    const rand = Math.floor(Math.random() * theMemes.length);
+    const url = theMemes[rand].url
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        randomImage: url,
+      };
+    });
+  }
 
-    return(
-        <div className="meme--container">
-            <div className="meme--input--container">
-                <input 
-                className="meme--topText"
-                type="text"
-                placeholder="Top Text"
-                ></input>
-                <input 
-                className="meme--bottomText"
-                type="text"
-                placeholder="Bottom Text"
-                ></input>
-            </div>
-            <div className="meme--button--container">
-                <button onClick={getMemeImage} className="meme--button">Get a new meme image  🖼 </button>
-            </div>
-            <img className="meme-img" src={memeImage} />
-        </div>
-    )
+  return (
+    <div className="meme--container">
+      <div className="meme--input--container">
+        <input
+          className="meme--topText"
+          type="text"
+          placeholder="Top Text"
+        ></input>
+        <input
+          className="meme--bottomText"
+          type="text"
+          placeholder="Bottom Text"
+        ></input>
+      </div>
+      <div className="meme--button--container">
+        <button onClick={getMemeImage} className="meme--button">
+          Get a new meme image 🖼{" "}
+        </button>
+      </div>
+      <img alt="meme" className="meme-img" src={meme.randomImage} />
+    </div>
+  );
 }
